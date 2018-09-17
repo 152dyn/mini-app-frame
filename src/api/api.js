c0e13d2d22430ebd0c5dc082180eaf152e8a6756
北有喬木 <beiyouqiaomu@Liu_Bin_QI_MAC.lan>
2018-09-17 18:14:23 +0800

    wx.request简单封装

--- src/api/api.js
+++ src/api/api.js
@@ -1,42 +1,47 @@
-import {
-  wxRequest
-} from '../utils/wxRequest'
+let API_URL = 'http://localhost/loverule/api/api.php'
 
-import wepy from 'wepy'
-
-// let env = '-test' // -dev 或者 -test
-const apiMall = 'https://wechatmall.abctaotao.com/testapi'
-// const apiMall = 'https://api.tangxinmao.com'
-let data = {
-  checkToken: '/user/session_status?token=',
+let requestHandler = {
+  params: {},
+  success: function(res) {
+    // success
+  },
+  fail: function() {
+    // fail
+  }
 }
 
-/**
- * 微信登录
- * @param  {[type]} params [description]
- * @return {[type]}        [description]
- */
-const wxLogin = (callback) => wepy.login({
-  success: function (res) {
-    console.log('wxLogin')
-    console.log(res)
-    if (typeof callback === 'function') {
-      callback(res)
-    }
-  },
-  fail: function (error) {
-    if (typeof error === 'object') {
-      console.log('登录失败：' + JSON.stringify(error))
-    } else {
-      console.log('登录失败：' + error)
-    }
-  }
-})
+// GET请求
+function GET(requestHandler) {
+  request('GET', requestHandler)
+}
+// POST请求
+function POST(requestHandler) {
+  request('POST', requestHandler)
+}
 
-// 提现
-const postMyCash = (params, callback) => wxRequest(params, apiMall + data.getMyCash, callback, 'POST')
-// 我的收支明细
+function request(method, requestHandler) {
+  // 注意：可以对params加密等处理
+  let params = requestHandler.params
+
+  wx.request({
+    url: API_URL,
+    data: params,
+    method: method, // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+    // header: {}, // 设置请求的 header
+    success: function(res) {
+      // 注意：可以对参数解密等处理
+      requestHandler.success(res)
+    },
+    fail: function() {
+      requestHandler.fail()
+    },
+    complete: function() {
+      // complete
+    }
+  })
+}
+
 module.exports = {
-  postMyCash,
-  wxLogin
+  GET: GET,
+  POST: POST
 }
